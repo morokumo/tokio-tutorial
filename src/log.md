f545bfaf89d89585d23bcdb4fc59c3d0c3ee52f7
morokumo <morokumo.lll@gmail.com>
2021-06-21 03:12:14 +0900

    Spawning（とりあえず）

--- src/log.md
+++ src/log.md
@@ -11,9 +11,12 @@
 - async fn はFuture traitを実装した無名の型を返す
 - async fn は .awaitによって初めて実行される．
 
-
-
 ## Spawining
+
+- 並行(Concurrency)と並列(Parallelism)の違い
+    - Concurrency は交互に作業を行うこと　実際の例では，CPUが行う作業がCocurrencyに相当する（実際はParallelismも行うが）．作業者が多くいるわけではない(GPUと比べて)が，1人が行う作業の速度が早いため（CPUの周波数），交互に作業を行ってもそれが成り立つ．
+    - Parallelism は２つ以上の作業者が同時に作業を行うこと　GPUではPrallelismであり，それは一つ一つのコアの処理性能が圧倒的に優れているわけではないが，コア(作業者)が多く(CPUと比べて)，同時に作業が行える．
+
 
 
 ## Shared state
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,74 @@
-use mini_redis::{Result, client};
+use std::{collections::HashMap, time::Duration};
+
+use mini_redis::{Command, Connection, Frame, Result, client::{self, connect}};
+use tokio::{net::{TcpListener, TcpStream}, task, time::sleep};
 
 #[tokio::main]
 pub async fn main()-> Result<()>{
+    let listener = TcpListener::bind("127.0.0.1:6379")
+    .await
+    .unwrap();
 
-    let mut client = client::connect("127.0.0.1:6379").await?;
-    client.set("hello", "world".into()).await?;
-
-    let result = client.get("hello").await?;
-    
-    println!("got value from the server; result ={:?}",result);
-
-    Ok(())
-
+    loop{
+        let (socket,_) = listener
+        .accept()
+        .await
+        .unwrap();
+        process(socket)
+        .await;
+        
+    }
 
 }
 
+async fn process(socket: TcpStream){
 
-// async fn say_world(){
-//     println!("world");
+    let mut db = HashMap::new();
+
+    let mut connection = Connection::new(socket);
+
+
+    while let Some(frame) = connection
+    .read_frame()
+    .await
+    .unwrap(){
+        
+        let response = match Command::from_frame(frame).unwrap(){
+            Command::Set(cmd) =>{
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }   
+            Command::Get(cmd)=>{
+                if let Some(value) = db.get(cmd.key()){
+                    Frame::Bulk(value.clone().into())
+                }else{
+                    Frame::Null
+                }
+            }  
+            cmd => panic!("unimplemented {:?}",cmd)
+        };
+        
+        connection.write_frame(&response)
+        .await
+        .unwrap();
+    }
+}
+
+
+// not use Tokio
+// use std::{io::{Read, Write}, net::{TcpListener, TcpStream}, thread::sleep, time::{self, Duration, SystemTime}};
+
+// fn main(){
+//     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+//     loop{
+//         let (socket,_) = listener.accept().unwrap();
+//         process(socket);
+
+//     }
+
 // }
 
-// #[tokio::main]
-// async fn main(){
-//     let op = say_world();
-
-//     println!("hello");
-
-//     op.await;    
+// pub fn process(mut socket:TcpStream){
+//     // println!("{:?}",SystemTime::now());
+//     sleep(Duration::new(10, 0));
 // }
